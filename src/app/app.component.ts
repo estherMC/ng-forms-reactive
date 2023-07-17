752f34b3f093bb671769389148841d962d5e7495
Esther Martorell <esther.martorell@allianz.com>
2023-07-17 14:40:54 +0200

    :sparkles: create a custom async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,11 @@ export class AppComponent implements OnInit {
   forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
-    //bind is needed because the method forbiddenNames will be called by Angular, no the property ts class
+    //bind is needed because the method forbiddenNames is calling this and the method will be called by Angular, no the property ts class
       this.signupForm = new FormGroup({
         'userData': new FormGroup({
           'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-          'email': new FormControl(null, [Validators.required, Validators.email]),
+          'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
         }),
         //FormControl --> arguments: first state, single validator, asynchronous validators
         'gender': new FormControl('female'),
@@ -48,6 +49,19 @@ export class AppComponent implements OnInit {
     }
     //Not {'nameIsForbidden' : false  } --> to tell Angular is invalid, we should send null
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject)=> {
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        }else{
+          resolve(null)
+        }
+      },  1500);
+    });
+    return promise;
   }
 
 
