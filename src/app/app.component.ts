6de9b261339937787997101be918f95ce9552857
Esther Martorell <esther.martorell@allianz.com>
2023-07-17 14:49:47 +0200

    :sparkles: set values of the form in ts and clean form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,12 +28,29 @@ export class AppComponent implements OnInit {
       // this.signupForm.valueChanges.subscribe(
         // (value) => console.log(value));
 
-      this.signupForm.statusChanges.subscribe(
-        (status) => console.log(status));
+      // this.signupForm.statusChanges.subscribe(
+      //   (status) => console.log(status));
+
+      this.signupForm.setValue({
+        'userData': {
+          'username': 'Esther',
+          'email': 'esther@test.com',
+        },
+        'gender': 'male',
+        'hobbies': []
+      });
+
+
+      this.signupForm.patchValue({
+        'userData': {
+          'username': 'Anna'
+        },
+      });
   }
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby(){
