2055b28217e624f058e9c4fb373006d5e8120195
Esther Martorell <esther.martorell@allianz.com>
2023-07-17 14:20:11 +0200

    :sparkles: create custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
+    //bind is needed because the method forbiddenNames will be called by Angular, no the property ts class
       this.signupForm = new FormGroup({
         'userData': new FormGroup({
-          'username': new FormControl(null, Validators.required),
+          'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           'email': new FormControl(null, [Validators.required, Validators.email]),
         }),
         //FormControl --> arguments: first state, single validator, asynchronous validators
@@ -39,5 +41,14 @@ export class AppComponent implements OnInit {
   // get controls() {
   //   return (this.signupForm.get('hobbies') as FormArray).controls;
   // }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden' : true};
+    }
+    //Not {'nameIsForbidden' : false  } --> to tell Angular is invalid, we should send null
+    return null;
+  }
+
 
 }
